2f001876d8d1d48ad675be05467cb50019c445b6
Aydana9008 <aydana_9008@yahoo.com>
2018-08-26 19:02:49 -0400

    Job

---
+++ job.js
@@ -0,0 +1,217 @@
+/*
+var var1 = prompt("Enter your fist name");
+var var2 = prompt("Enter your last name");
+var var3 = prompt("Enter your age");
+console.log("Hi, my name is " + var1 + " " + var2 + ", and my age is " + var3 + ". Nice to meet you !");
+var marryAge = 23;
+if((var3 - marryAge) <= 3) {
+  console.log(var1 + " You gonna marry very soon!");
+}
+else if((var3 - marryAge) > 4) {
+	console.log(var1 + " You need to think about studying well in school!");
+}
+else {
+	console.log(var1 + " You are gonna stay lonely, if you wont find a husband very soon!");
+}
+
+*/
+
+/*var askName = prompt("Enter your name");
+
+var askAge = prompt("Enter your birth year");
+
+var calc = 2018 - askAge;
+
+console.log("Hello " + askName + " ! Based on your input, your age is " + calc);
+
+
+
+var ask = prompt("Enter your fave number");
+
+var calc = ask%2;
+
+console.log("In case there is a remainder other than 0, your number is odd. Otherwise your number is even.")
+console.log("You number is = " + calc);
+
+var radius = parseInt(prompt("Enter the radius of the circle I will calculate the area for you"));
+var calc = radius * 3.14 * radius;
+console.log("You circle radius is " + radius + " and the area of the circle is " + calc);
+
+
+
+var num1, num2, operator, blank;
+num1 = parseInt(prompt("Enter first number"));
+num2 = parseInt(prompt("Enter second number"));
+operator = prompt("Pick one of these operators +,-,*,/");
+var nothing = '';
+if (num1 === 0 || num2 === 0) {
+    alert("Please enter valid numbers");
+}   
+
+if (operator==="+"){
+    console.log(num1+num2);
+}
+
+else if(operator === "-"){
+    console.log(num1 - num2);
+}
+else if(operator=== "*"){
+    console.log(num1 * num2);
+}
+
+else{
+    console.log(num1 / num2);
+}
+*/
+
+
+
+
+/*
+var even, zero, ask;
+ask = parseInt(prompt("Enter any number"));
+zero = 0;
+even = ask%2;
+
+
+switch(zero) {
+case  even:
+	console.log(ask + " number is even");
+	break;
+   default :
+   console.log(ask + " number is odd");
+}
+
+
+*/
+
+
+
+
+
+/*
+var resp = parseInt(prompt("Enter your fave number"));
+var low0 = 0;
+var low1 = 1;
+var low2 = 2;
+var med3 = 3;
+var med4 = 4;
+var med5 = 5; 
+
+switch(resp){
+case low0 :
+console.log("Low number!");
+break;
+case low1 :
+console.log("Low number!");
+break;
+case low2 :
+console.log("Low number!");
+break;
+case med3 :
+console.log("Medium number!");
+break;
+case med4 :
+console.log("Medium number!");
+break;
+case med5 :
+console.log("Medium number!");
+break;
+default :
+console.log("Other number!");
+}
+*/
+
+
+/*
+
+var total;
+
+var price = parseInt(prompt("Enter the total amount for today's shopping"));
+
+switch(true){
+	case (price <= 1000) :
+    total = price + price * 0.05;
+    console.log("$" + total + " is the total amount after discount!");
+    break;
+case (price <= 10000) :
+    total = price + price * 0.08;
+    console.log("$" + total + " is the total amount after discount!");
+    break;
+default :
+     price;
+    console.log("$" + price + " is the total amount!");
+}
+
+
+*/
+
+
+
+
+var odd, even;
+var a=prompt("Enter even number(over 2), I will check for you if it is even or odd, by true or false.");
+var b=prompt("Enter odd number(over 3), I will check for you if it is even or odd, by true or false.")
+a=a%2==0;
+b=b%3==0;
+console.log(a + " This is even number. ");
+console.log(b + " This is odd number. ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
